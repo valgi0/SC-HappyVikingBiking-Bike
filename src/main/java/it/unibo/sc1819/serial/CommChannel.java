6a0c08c742d5b3fec16313068654ad08c072d3f5
Tirocigno <fede.naldini1@gmail.com>
2019-05-15 15:40:53 +0200

    Imported serial handler java code

---
+++ src/main/java/it/unibo/sc1819/serial/CommChannel.java
@@ -0,0 +1,34 @@
+package it.unibo.sc1819.serial;
+
+/**
+ * Simple interface for an async msg communication channel
+ * 
+ * @author aricci
+ *
+ */
+public interface CommChannel {
+
+    /**
+     * To send an message.
+     * 
+     * Asynchronous model.
+     * 
+     * @param msg
+     */
+    void sendMsg(String msg);
+
+    /**
+     * To receive a message.
+     * 
+     * Blocking behaviour.
+     */
+    String receiveMsg() throws InterruptedException;
+
+    /**
+     * To check if a message is available.
+     * 
+     * @return
+     */
+    boolean isMsgAvailable();
+
+}
