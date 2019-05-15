06c96cb56db5c7581c11d713112d1d7d975c1a32
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2019-05-15 18:17:07 +0200

    Check photoresistor

---
+++ sensors/photoresistor/Phtoresistor/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+/*
+Resistore collegato a 5v e all'ingresso analogico.
+In più tra il resistore e l'inresso montiamo una resistenza
+di 100H che lo collega alla massa
+*/
+
+int photo = A0;
+int led = 12;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int light = analogRead(photo);
+  Serial.write("Luce: ");
+  Serial.println(light);
+  if(light < 8){
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+  delay(500);
+}
